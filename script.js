a13b2129f5449caf0756efbe9eee58f70de6e126
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2022-11-17 16:41:16 +0300

    Create calculate and findPrecedent functions

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 "use strict";
 
-function operPressed() {
+function calculate() {
+    const numberOfOperations = operArray.length;
+    for(let i = 0; i < numberOfOperations; i++) {
+        operate();
+    }
+}
+
+function operate() {
+    //
+}
+
+function findPrecedentFunction() {
+
+}
+
+function getOper() {
     const currentIndex = inputLine.textContent.length - 1;
     const currentOperator = inputLine.textContent[currentIndex];
     operArray.push(currentOperator);
@@ -12,6 +27,13 @@ function getNum(currentIndex) {
     previousIndex = currentIndex;
 }
 
+
+const PRECEDENCEDICT = {
+    "+": 1,
+    "-": 1,
+    "x": 2,
+    "÷": 2,
+};
 const inputLine = document.querySelector("h2");
 const resultLine = document.querySelector("h3");
 const numArray = [];
