e88f83035745cd9de93279b747de6ab29ce2e8d7
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2022-11-17 16:53:24 +0300

    Create operator function

--- script.js
+++ script.js
@@ -8,11 +8,34 @@ function calculate() {
 }
 
 function operate() {
-    //
+    // TODO: Disable equals button being used right after an operator is used as there will be no number after the operation
+    const operationIndex = findPrecedentIndex()
+    const currentOperation = operArray[operationIndex];
+    const firstNumber = numArray[operationIndex];
+    const secondNumber = numArray[operationIndex + 1];
+    if (currentOperation === "+") {
+        numArray[currentIndex] = firstNumber + secondNumber;
+    } else if (currentOperation === "-") {
+        numArray[currentIndex] = firstNumber - secondNumber;
+    } else if (currentOperation === "x") {
+        numArray[currentIndex] = firstNumber * secondNumber;
+    } else if (currentOperation === "÷") {
+        numArray[currentIndex] = firstNumber / secondNumber;
+    }
+    // Below line is to remove second number. First one is replaced by the result. This is necessary as we get one result out
+    // of two number inputs.
+    numArray.splice((currentIndex + 1), 1);
 }
 
-function findPrecedentFunction() {
-
+function findPrecedentIndex() {
+    const precendentIndex = 0;
+    const currentHighestPrecendence = 0;
+    for (let i = 0; i < operArray.length; i++) {
+        if (PRECEDENCEDICT[operArray[i]] > currentHighestPrecendence) {
+            precendentIndex = i;
+        }
+    }
+    return precendentIndex;
 }
 
 function getOper() {
