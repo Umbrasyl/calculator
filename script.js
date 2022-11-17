3800a8a43eb7a04f00fe9862eae1a230f7c80a17
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2022-11-17 16:32:55 +0300

    Create number storing and operator storing functions

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 "use strict";
 
+function operPressed() {
+    const currentIndex = inputLine.textContent.length - 1;
+    const currentOperator = inputLine.textContent[currentIndex];
+    operArray.push(currentOperator);
+    getNum(currentIndex);
+}
 
-
-function getNum(firstIndex, secondIndex) {
-
+function getNum(currentIndex) {
+    numArray.push(+inputLine.textContent.slice(previousIndex, currentIndex));
+    previousIndex = currentIndex;
 }
 
 const inputLine = document.querySelector("h2");
 const resultLine = document.querySelector("h3");
 const numArray = [];
 const operArray = [];
+let previousIndex = 0;
