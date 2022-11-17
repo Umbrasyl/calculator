bc573d37bb518cac05ffb1e37bfe6cc0dcd4142d
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2022-11-17 16:12:48 +0300

    Create the buttons and basic layout.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 "use strict";
 
 
+
+function getNum(firstIndex, secondIndex) {
+
+}
+
+const inputLine = document.querySelector("h2");
+const resultLine = document.querySelector("h3");
+const numArray = [];
+const operArray = [];
